5a833cdaff58eddb67d3ff45931d2596a5b7877d
Romario Melo <romariomelo360@gmail.com>
2026-06-05 01:43:46 -0300

    feat(api): Add base repo no auth

--- src/main/java/com/siti/sitiapi/repository/AuthRepository.java
+++ src/main/java/com/siti/sitiapi/repository/AuthRepository.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 @Repository
 @RequiredArgsConstructor
-public class AuthRepository {
+public class AuthRepository implements BaseRepository{
 
     private final JdbcTemplate jdbcTemplate;
 
